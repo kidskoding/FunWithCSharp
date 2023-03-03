efff9923bfdbb51f68b1b6a4af03ad3cae74932c
Anirudh Konidala <kidskodingclub@gmail.com>
2023-03-03 11:47:15 -0600

    test commit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ namespace FunWithCSharp {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine((int) 'D');
+            Console.WriteLine("Hello!");
         }
+        public static int add(int x, int y) {
+            return x + y;
+        }
+        public 
     }
 }
